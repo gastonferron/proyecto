542f3dcaa7eb2b3f113a4916329ccdce3f123508
gaston_ferron <bruceferr@gmail.com>
2022-08-21 20:32:27 -0300

    Productos

--- js/products.js
+++ js/products.js
@@ -1,25 +1,23 @@
-let currentCategoriesArray = [];
+let currentProductsArray = [];
 
-function showCategoriesList(){
+function showProductsList(){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentCategoriesArray.products.length; i++){
-        let category = currentCategoriesArray.products[i];
-
-        console.log(category);
+    for(let i = 0; i < currentProductsArray.products.length; i++){
+        let productos = currentProductsArray.products[i];
 
             htmlContentToAppend += `
             <div class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
-                        <img src="${category.image}" alt="${category.description}" class="img-thumbnail">
+                        <img src="${productos.image}" alt="${productos.description}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name} - ${category.currency} ${category.cost}</h4>
-                            <small class="text-muted">${category.soldCount} artículos</small>
+                            <h4 class="mb-1">${productos.name} - ${productos.currency} ${productos.cost}</h4>
+                            <small class="text-muted">${productos.soldCount} ventas</small>
                         </div>
-                        <p class="mb-1">${category.description}</p>
+                        <p class="mb-1">${productos.description}</p>
                     </div>
                 </div>
             </div>
@@ -31,8 +29,8 @@ function showCategoriesList(){
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
-            currentCategoriesArray = resultObj.data        
-            showCategoriesList();
+            currentProductsArray = resultObj.data        
+            showProductsList();
         }
     });
 
