1286fe4a2b6b04bb7bc21703835a19546c747257
gastonferron <bruceferr@gmail.com>
2022-10-30 23:26:16 -0300

    Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,28 +1,77 @@
 let currentCartInfo = "";
 let valor="";
-function showCart(){       
-    let htmlContentToAppend = `
+let formulario = document.getElementById("form");
 
-    <div class="cart-row">
-        <div class="cart-item cart-column">
-            <img src="${currentCartInfo.articles[0].image}"class="img-thumbnail" width="300" height="100></img>
-            <span class="cart-item-title">  ${currentCartInfo.articles[0].name}</span>
-        </div>
-        <span class="cart-price cart-column">${currentCartInfo.articles[0].currency}${currentCartInfo.articles[0].unitCost}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" value="${currentCartInfo.articles[0].count}">
-            ${cart_Subtotal()}
-         </div>
-    </div>
-        <div class="cart-total">
-            <strong class="cart-total-title">Precio Final ${currentCartInfo.articles[0].currency} ${cart_Subtotal()}</strong>
-            <span class="cart-total-price"></span>
-        </div>
+function showCart(){     
+    let htmlContentToAppend="";
+    
+        for (let i = 0; i < currentCartInfo.articles.length; i++) {
         
+            htmlContentToAppend += `
+
+                <table class="w-100">
+                    <tr>
+                        <th>#</th>
+                        <th>Productos</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Subtotal</th>
+                    </tr>
+                    <tr>
+                        <td><img class="img-thumbnail img-article" src="${currentCartInfo.articles[i].image}"></img></td>
+                        <td><label>${currentCartInfo.articles[i].name}</label></td>
+                        <td><label>${currentCartInfo.articles[i].currency}${currentCartInfo.articles[i].unitCost}</label></td>
+                        <td>
+                        <div class="d-flex justify-content-center">
+                            <div class="d-flex ">
+                                <button class="btn" onclick="disminuirArticulos(${i})"><i class="fa fa-minus"></i></button>
+                                <input class="form-control" value="${currentCartInfo.articles[i].count}" disabled>
+                                <button class="btn" onclick="aumentarArticulos(${i})"><i class="fa fa-plus"></i></button>
+                            </div>
+                        </div>      
+                        </td>
+                        <div class="min-vw-50">
+                            <td><label>${currentCartInfo.articles[i].currency} ${cart_Subtotal()}</label></td>
+                        </div>
+                    </tr>
+                </table>
         `
+        }  
 
         document.getElementById("lista_currentCartInfo").innerHTML = htmlContentToAppend;
 }
+
+function showCostos(){
+    let htmlCostos="";
+    
+    for (let i = 0; i < currentCartInfo.articles.length; i++) {
+    
+        htmlCostos += `
+    <div class="list-group-item list-group-item-action cursor-active">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">Subtotal</h4>
+                    <small class="text-muted">${currentCartInfo.articles[i].currency} ${cart_Subtotal()}</small>
+                </div>
+                <p class="mb-1">Costo unitario del producto por cantidad</p>
+            </div>
+        </div>
+    </div>    
+            `}
+        document.getElementById("lista_Costos").innerHTML = htmlCostos;
+}
+
+
+    function disminuirArticulos(i){
+        currentCartInfo.articles[i].count = Math.max(currentCartInfo.articles[i].count - 1 , 1);
+        showCart();
+    }
+
+    function aumentarArticulos(i){
+        currentCartInfo.articles[i].count = currentCartInfo.articles[i].count + 1;
+        showCart();
+    }
 
     function cart_Subtotal(){
         return valor= currentCartInfo.articles[0].unitCost * currentCartInfo.articles[0].count;
@@ -32,10 +81,27 @@ function showCart(){
         total=cart_Subtotal;
     }
 
+    (function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
     getJSONData(CART_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             currentCartInfo = resultObj.data 
-            console.log(currentCartInfo);     
             showCart();
+            showCostos()
+
         }
     }); 
